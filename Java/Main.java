2587fb1078a9cd962425f6f3e641dacf1c4f9da6
Ivansyah <ivansyahsantoso@gmail.com>
2024-12-02 09:48:12 +0700

    coba saja

---
+++ Java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String[] mobil = {"Toyota", "Mobil Ghoib (Esemka)", "Suzuki"};
+        System.out.println(mobil[1]);
+        System.out.println(mobil[2]);
+    }
+}
